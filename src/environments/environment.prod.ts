58e509dc4acdf40eaf64f5515bcd735f26b9a795
henrygustavo <henrygustavof@gmail.com>
2018-10-14 01:50:51 -0500

    fixing prod url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl : 'https://shoppingcartgateway-courteous-crane.cfapps.io/'
+  apiUrl : 'https://shoppingcartgateway-terrific-duiker.cfapps.io/'
 };
